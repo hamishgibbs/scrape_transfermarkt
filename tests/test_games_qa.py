758c49699830c154702cc85ac3e6bfac421e97cf
Hamish Gibbs <hamishgibbs@gmail.com>
2023-04-19 14:12:47 +0100

    qa test games

--- tests/test_games_qa.py
+++ tests/test_games_qa.py
@@ -8,4 +8,26 @@ def test_all_teams_are_in_teams():
     print(len(list(set(games["home_team"]))))
 
     assert set(games["home_team"]) - set(teams["url_stub"]) == set()
+    assert set(teams["url_stub"]) - set(games["home_team"]) == set()
+    assert set(games["away_team"]) - set(teams["url_stub"]) == set()
+    assert set(teams["url_stub"]) - set(games["away_team"]) == set()
     
+def test_all_attendances_are_lte_max_attendance_per_stadium():
+
+    # Maximum level of overselling allowed
+    MAX_ATTENDANCE_THRESHOLD = 1.01
+
+    games = pd.read_csv("data/games.csv")
+    stadiums = pd.read_csv("data/stadiums.csv")
+
+    games = pd.merge(games, stadiums, how="left", left_on="home_team", right_on="team")
+
+    games.dropna(subset=["attendance"], inplace=True)
+
+    pd.set_option('display.max_columns', None)
+
+    games["attendance_proportion"] = (games["attendance"] - games["capacity"]) / games["capacity"]
+
+    print(games[~(games["attendance_proportion"] <= MAX_ATTENDANCE_THRESHOLD)])
+
+    assert games["attendance_proportion"].max() <= MAX_ATTENDANCE_THRESHOLD
